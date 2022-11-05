570490b3ffcb2825168949ab3e3d87bb8fa4f39f
Shahed1998 <shahedchowdhury120@gmail.com>
2022-11-05 13:22:23 +0600

    ...

---
+++ server/Controllers/EmployeeController.cs
@@ -0,0 +1,84 @@
+﻿using server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace server.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public ActionResult All(Zero_hungerEntities1 db)
+        {
+            var employees = db.Employees.ToList();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(Zero_hungerEntities1 db, Employee employee, string Name)
+        {
+            employee.Name = Name;
+            employee.isAvailable = 1;
+            db.Employees.Add(employee);
+            db.SaveChanges();
+            return RedirectToAction("All");
+        }
+
+        [HttpGet]
+        public ActionResult Details(Zero_hungerEntities1 db, int id)
+        {
+            var employee = (from row in db.Employees
+                            where row.Id == id
+                            select row).SingleOrDefault();
+            return View(employee);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Zero_hungerEntities1 db, int id)
+        {
+            var employee = (from row in db.Employees
+                            where row.Id == id
+                            select row).SingleOrDefault();
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Zero_hungerEntities1 db, Employee employee, string Name, int Id)
+        {
+            
+
+            var employee2 = (from row in db.Employees
+                             where row.Id == Id
+                             select row).SingleOrDefault();
+
+            employee.Name = Name;
+            employee.isAvailable = employee2.isAvailable;
+
+            db.Entry(employee2).CurrentValues.SetValues(employee);
+            db.SaveChanges();
+            TempData["editMsg"] = "Employee update successful";
+            return RedirectToAction("Edit", new {id = employee2.Id});
+        }
+
+        [HttpGet]
+        public ActionResult Delete(Zero_hungerEntities1 db, int id)
+        {
+            var employee = (from row in db.Employees
+                            where row.Id == id
+                            select row).FirstOrDefault();
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("All");
+        }
+
+    }
+}
--- server/Controllers/RestaurantController.cs
+++ server/Controllers/RestaurantController.cs
@@ -9,6 +9,28 @@ namespace server.Controllers
 {
     public class RestaurantController : Controller
     {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Zero_hungerEntities1 db, string Name)
+        {
+            var restaurant = (from row in db.Restaurants
+                              where row.Name == Name
+                              select row).SingleOrDefault();
+
+            if (restaurant != null)
+            {
+                Session["RestaurantId"] = restaurant.Id;
+                return RedirectToAction("All");
+            }
+
+            TempData["loginMsg"] = "User not found";
+            return RedirectToAction("Login");
+        }
 
         [HttpGet]
         public ActionResult All(Zero_hungerEntities1 db)
@@ -49,6 +71,38 @@ namespace server.Controllers
             }
 
             return RedirectToAction("All");
+        }
+
+        [HttpGet]
+        public ActionResult Details(Zero_hungerEntities1 db, int id)
+        {
+            var res = (from row in db.Restaurants
+                       where row.Id == id
+                       select row).SingleOrDefault();
+            return View(res);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(Zero_hungerEntities1 db, int id)
+        {
+            var res = (from row in db.Restaurants
+                       where row.Id == id
+                       select row).SingleOrDefault();
+            return View(res);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Zero_hungerEntities1 db, Restaurant restaurant)
+        {
+            var ext = (from row in db.Restaurants
+                    where row.Id == restaurant.Id
+                    select row
+                ).FirstOrDefault();
+
+            db.Entry(ext).CurrentValues.SetValues(restaurant);
+            db.SaveChanges();
+            TempData["editMsg"] = "Updated successfully";
+            return RedirectToAction("Edit");
         }
     }
 }
